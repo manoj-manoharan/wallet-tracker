55e7a9d1c640998639b074e2941723cf98b84d74
manoj-manoharan <yogeshwaranmanoharan@gmail.com>
2022-03-26 23:57:11 +0530

    - App setup - Home page layout partially done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({Component, pageProps}: AppProps) {
+  return (
+      <>
+        <Head>
+          <meta name="viewport"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
+          <title>Wallet Tracker</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
-export default MyApp
+export default App
