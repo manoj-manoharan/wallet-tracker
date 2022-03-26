7efe143defa900eec7f7fe29bee798040041dd5a
manoj-manoharan <yogeshwaranmanoharan@gmail.com>
2022-03-27 01:21:36 +0530

    - Partially created page create for activity

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,6 +1,22 @@
+import styles from "../styles/Create.module.css";
+import Link from "next/link";
+
 const Create = () => {
     return (
-        <h1>Create</h1>
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.heroText}>
+                    <h4>Wallet Tracker</h4>
+                </div>
+            </div>
+            <div className={styles.bottom}>
+                <button className={styles.createButton}>Create</button>
+                <div className={styles.activityTypesContainer}>
+                    <h3 className={styles.activeActivityType}>Income</h3>
+                    <h3>Expense</h3>
+                </div>
+            </div>
+        </div>
     );
 }
 
